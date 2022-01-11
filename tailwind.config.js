1c0b5a6437772f906f2ca517c4c2ff197f36f97e
Allison Schulman <allison.d.schulman@gmail.com>
2022-01-11 17:33:28 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkgreen: '#052324',
+        graygreen: '#7B9291',
+        palegreen: '#E8FAF3',
+        lightgray: '#FFFFFF',
+      }
+    },
+  },
+  plugins: [],
+}
