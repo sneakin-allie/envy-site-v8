49c75211fd3d960ac6829efef62b15c9381a3f8a
Allison Schulman <allison.d.schulman@gmail.com>
2022-01-12 09:04:23 -0500

    Imported & applied Google Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         graygreen: '#7B9291',
         palegreen: '#E8FAF3',
         lightgray: '#FFFFFF',
+      },
+      fontFamily: {
+        ibmplexserif: ['IBM Plex Serif'],
+        spacegrotesk: ['Space Grotesk']
       }
     },
   },
