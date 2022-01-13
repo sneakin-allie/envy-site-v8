fc249db1d0f99f817a116967de0e992c3995c3a7
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2022-01-13 12:56:22 -0500

    Updated font families/sizes/colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,29 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        darkgreen: '#052324',
-        graygreen: '#7B9291',
-        palegreen: '#E8FAF3',
-        lightgray: '#FFFFFF', // change this to white
-        darkergreen: '#041F20',
+      colors: { // taken rom Ayana's color scheme
+        darkgreen: '#052324', // formerly darkgreen
+        teal: '#047870',
+        emerald: '#1DCA7F',
+        aquamarine: '#7EE8B9',
+        mint: '#BEF0DB',
+        palemint: '#E8FAF3', // formerly palegreen - updated
+        darkslategray: '#314448',
+        gunmetalgray: '#455C5A', // formerly darkslategray - updated
+        slategray: '#7B9291', // formerly graygreen - updated
+        silver: '#BDC8C8',
+        darkergreen: '#041F20', // formerly darkergreen NOT in color scale
       },
       fontFamily: {
-        ibmplexserif: ['IBM Plex Serif'],
-        spacegrotesk: ['Space Grotesk']
+        ibmplexserif: ['IBM Plex Serif'], // formerly ibmplexserif
+        spacegrotesk: ['Space Grotesk'], // formerly spacegrotesk
+      },
+      fontSize: {
+        'h3': ['2.5rem'],
+        '*': ['5.625rem'],
+        'larger': ['9.375rem'],
+        'largest': ['14.063rem'],
+        // '2xl': ['1.5rem', { lineHeight: '2rem' }],
       }
     },
   },
